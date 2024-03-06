37bbc959601390eeaf06b6331c0f6f648ad9ff69
Fernando David Medvedovsky <fernandodavidmedve56@gmail.com>
2024-03-06 11:12:48 -0300

    Agregamos resize de pantalla

--- src/index.ts
+++ src/index.ts
@@ -16,19 +16,40 @@ const app = new Application<HTMLCanvasElement>({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
+
+window.addEventListener("resize", () => {
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+});
+
+window.dispatchEvent(new Event("resize"));
 
 init().then(() => {
 	const clampy: Sprite = Sprite.from("Dino the dinosaur");
 
 	console.log("Hola mundo!", clampy.width, clampy.height);
 
-	//clampy.anchor.set(0.5);
-
-	clampy.x = 300;
-	clampy.y = 300;
+	clampy.x = 0;
+	clampy.y = 0;
 
 	app.stage.addChild(clampy);
 });
