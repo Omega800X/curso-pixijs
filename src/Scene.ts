0cb49686bea765c2fd9881cf96bfa534be787bec
Fernando David Medvedovsky <fernandodavidmedve56@gmail.com>
2024-03-07 11:41:59 -0300

    Pruebas con AnimatedSprite, Graphics, Text y NineSlicePlane

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,4 +1,4 @@
-import { Container } from "pixi.js";
+import { AnimatedSprite, Container, Graphics, NineSlicePlane, Text, Texture } from "pixi.js";
 import { DinoHat } from "./DinoHat";
 import { DogBall } from "./DogBall";
 import { DogHat } from "./DogHat";
@@ -8,23 +8,97 @@ export class Scene extends Container {
     constructor() {
         super();
         
-        const dinoWithHat: Container = new DinoHat();
+        // Class extending from Container
+        const dinoWithHat: DinoHat = new DinoHat();
         dinoWithHat.scale.set(0.5);
         dinoWithHat.x = 200;
         dinoWithHat.y = 300;
 
-        const dogWithBall: Container = new DogBall();
+        const dogWithBall: DogBall = new DogBall();
         dogWithBall.scale.set(0.8);
         dogWithBall.position.set(1000, 500);
         dogWithBall.angle = -90;
     
-        const dogWithHat: Container = new DogHat();
+        const dogWithHat: DogHat = new DogHat();
         dogWithHat.position.set(640, 360);
         dogWithHat.scale.set(0.5);
         dogWithHat.rotation = Math.PI;
 
+        // Animated sprite
+        const movingChar: AnimatedSprite = new AnimatedSprite(
+            [
+                Texture.from("CharW0"),
+                Texture.from("CharW1"),
+                Texture.from("CharW2"),
+                Texture.from("CharW3"),
+                Texture.from("CharW4"),
+                Texture.from("CharW5"),
+                Texture.from("CharW6"),
+                Texture.from("CharW7"),
+            ], 
+            true
+        );
+
+        movingChar.scale.set(4);
+        movingChar.play();
+        movingChar.animationSpeed = 0.25;
+
+        // Graphics
+        const myGraph: Graphics = new Graphics();
+        myGraph.lineStyle(
+            {
+                color: 0xFF00FF, 
+                width: 10, 
+                alpha: 1
+            }
+        );
+        myGraph.moveTo(0, 0);
+        myGraph.lineTo(300, 500);
+        myGraph.lineTo(300, 100);
+        myGraph.lineTo(0, 0);
+        
+        myGraph.clear();
+
+        myGraph.lineStyle(
+            {
+                color: 0xFF00FF, 
+                width: 10, 
+                alpha: 1
+            }
+        );
+        myGraph.beginFill(0x00FF00, 1);
+        myGraph.drawCircle(0, 0, 100);
+        myGraph.position.set(640, 360);
+        myGraph.endFill();
+        myGraph.drawCircle(50, 50, 100);
+
+        // Text, not the best practice, better ones in other videos
+        const myText: Text = new Text("Hello World", {fontSize: 150, fill: 0xFF0000, fontFamily: "D050000L"});
+
+        myText.text = "asdsdaasd";
+        myText.angle = 40;
+        myText.scale.set(1);
+        myText.position.x = 500;
+
+        // Nine-Slice Plane
+        //const panel: Sprite = Sprite.from("Tile");
+        //panel.scale.set(5, 3);
+        const panel: NineSlicePlane = new NineSlicePlane(
+            Texture.from("Tile"),
+            35, 35, 35, 35
+        );
+        panel.width = 500;
+        panel.height = 300;
+        panel.scale.set(2);
+        panel.position.x = 100;
+        panel.position.y = 100;
+
         this.addChild(dinoWithHat);
         this.addChild(dogWithBall);
 	    this.addChild(dogWithHat);
+        this.addChild(movingChar);
+        this.addChild(myGraph);
+        this.addChild(myText);
+        this.addChild(panel);
     }
 }
--- src/assets.ts
+++ src/assets.ts
@@ -11,6 +11,15 @@ export const manifest:AssetsManifest = {
                 "Dinohat": "./hat.png",
                 "Dog": "./dog.png",
                 "Basketball": "./ball.png",
+                "CharW0": "./animated_char/char000.png",
+                "CharW1": "./animated_char/char001.png",
+                "CharW2": "./animated_char/char002.png",
+                "CharW3": "./animated_char/char003.png",
+                "CharW4": "./animated_char/char004.png",
+                "CharW5": "./animated_char/char005.png",
+                "CharW6": "./animated_char/char006.png",
+                "CharW7": "./animated_char/char007.png",
+                "Tile": "./tile.png",
             }
         },
     ]
